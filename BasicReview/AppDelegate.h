be208b9809deb5be36e5325547207de6c6510fdb
Zhaoyang Yu <NearMilk@users.noreply.github.com>
2017-11-17 15:32:44 +0800

    Initial Commit

---
+++ BasicReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicReview
+//
+//  Created by near on 2017/11/17.
+//  Copyright © 2017年 near. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
